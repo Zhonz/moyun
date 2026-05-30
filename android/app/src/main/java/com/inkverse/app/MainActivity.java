3a7f9547d79aa97b97f35544e09361e1c076259d
GitHub User <user@github.com>
2026-05-30 14:38:02 +0000

    feat: 实现安卓沉浸式体验模式
    - 添加沉浸式 sticky 模式，自动隐藏虚拟导航栏
    
    - 优化系统栏透明背景
    
    - 适配安全边距，防止内容被系统栏遮挡

--- android/app/src/main/java/com/inkverse/app/MainActivity.java
+++ android/app/src/main/java/com/inkverse/app/MainActivity.java
@@ -1,5 +1,34 @@
 package com.inkverse.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableImmersiveMode();
+    }
+    
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableImmersiveMode();
+        }
+    }
+    
+    private void enableImmersiveMode() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
